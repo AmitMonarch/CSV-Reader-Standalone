d1977aa6e819fb78c5748744f9dd6dd771388c30
Amitesh Kumar B A <167799371+AmitMonarch@users.noreply.github.com>
2024-11-04 13:58:05 +0530

    API_URL udate for non docker deployment.

--- csv-uploader/src/App.tsx
+++ csv-uploader/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import CsvUploader from './components/CsvUploader';
 
+// const API_URL = 'https://localhost:7174/api/Csv/upload';
+// Running without docker comment the below line and uncomment the above line.
 const API_URL = 'http://localhost:7174/api/Csv/upload';
 
 function App() {
